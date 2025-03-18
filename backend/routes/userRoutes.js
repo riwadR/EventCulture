d784035da9f4b679601a53bb317adc7830f9950b
Loris Poilly <pcacerloris@gmail.com>
2025-03-18 15:01:11 +0100

    Login / Register a l'arache

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/UserController");
 
 router.post("/new", userController.createUser); // Créer un utilisateur
+router.post("/login", userController.login); // Connexion d'un utilisateur
 router.get("/users", userController.getAllUsers); // Récupérer tous les utilisateurs
 router.get("/:id", userController.getUserByPk); // Récupérer un utilisateur par ID
 router.put("/:id", userController.updateUser); // Mettre à jour un utilisateur
