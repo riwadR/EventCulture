a92484e1f42ac0ab5b055768bb7becf4655b5b5f
RiwadR <riwadrakotoarivao@gmail.com>
2025-03-24 11:06:58 +0100

    add Rgister + Login

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,36 +1,33 @@
-import React from 'react';
-import { useNavigate } from 'react-router';
-import './Navbar.css';
+import React from "react";
+import { useNavigate } from "react-router";
+import "./Navbar.css";
 
 const Navbar = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const buttons = [
+    { name: "Accueil", path: "/" },
+    { name: "Register", path: "/register" },
+    { name: "login", path: "/login" },
+    { name: "Catalogue", path: "/catalogue" },
+    { name: "Event", path: "/event" },
+  ];
 
-    const buttons = [
-        { name: 'Accueil', path:'/' },
-        { name: 'Formulaire', path:'/formulaire' },
-        { name: 'Catalogue', path:'/catalogue' },
-        { name: 'Event', path:'/event' },
-    ];
+  const handleClick = (path) => {
+    navigate(path);
+  };
 
-    const handleClick = (path) => {
-        navigate(path);
-    };
-
-    return (
-        <div className="navbar">
-            <nav>
-                {buttons.map((button, index) => (
-                    <button
-                        key={index}
-                        onClick={() => handleClick(button.path)}
-                    >
-                        {button.name}
-                    </button>
-                ))}
-            </nav>
-        </div>
-    );
+  return (
+    <div className="navbar">
+      <nav>
+        {buttons.map((button, index) => (
+          <button key={index} onClick={() => handleClick(button.path)}>
+            {button.name}
+          </button>
+        ))}
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
