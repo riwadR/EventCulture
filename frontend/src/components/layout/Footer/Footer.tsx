ffa0325822a9a630d968faa788ff80513b7e030a
RiwadR <riwadrakotoarivao@gmail.com>
2025-05-13 11:56:46 +0200

    Add social network link

--- frontend/src/components/layout/Footer/Footer.tsx
+++ frontend/src/components/layout/Footer/Footer.tsx
@@ -2,18 +2,23 @@ import React from "react";
 import { useNavigate } from "react-router";
 import "./Footer.scss";
 
-interface FooterButton {
-  name: string;
-  path: string;
-}
-
-const Footer: React.FC = () => {
+const Footer = () => {
   const navigate = useNavigate();
 
-  const buttons: FooterButton[] = [
-    { name: "À propos", path: "/about" },
-    { name: "Contact", path: "/contact" },
-    { name: "Mentions légales", path: "/legal" },
+  const buttons = [
+    { name: "À propos", path: "/about", external: false },
+    { name: "Contact", path: "/contact", external: false },
+    { name: "Mentions légales", path: "/legal", external: false },
+    {
+      name: "Facebook",
+      path: "https://www.facebook.com/share/16BZAfML6D/",
+      external: true,
+    },
+    {
+      name: "Instagram",
+      path: "https://www.instagram.com/agir_villages_aures?igsh=ZjE2aTRkMHQ3bjR4",
+      external: true,
+    },
   ];
 
   return (
@@ -23,11 +28,22 @@ const Footer: React.FC = () => {
           &copy; {new Date().getFullYear()} Eventculture. Tous droits réservés.
         </p>
         <div className="footer-links">
-          {buttons.map((button, index) => (
-            <button key={index} onClick={() => navigate(button.path)}>
-              {button.name}
-            </button>
-          ))}
+          {buttons.map((button, index) =>
+            button.external ? (
+              <a
+                key={index}
+                href={button.path}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {button.name}
+              </a>
+            ) : (
+              <button key={index} onClick={() => navigate(button.path)}>
+                {button.name}
+              </button>
+            )
+          )}
         </div>
       </div>
     </footer>
